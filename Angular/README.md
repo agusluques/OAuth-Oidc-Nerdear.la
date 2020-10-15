ac799ede0cbb68cd987e8c6b62fa3e8076421179
Agustin Luques <luquesagustin@gmail.com>
2020-10-15 15:19:38 -0300

    change to nerdearla

--- Angular/README.md
+++ Angular/README.md
@@ -1,4 +1,4 @@
-# pw-angular
+# nerdearla-angular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.0.
 
--- Angular/src/app/auth.config.ts
+++ Angular/src/app/auth.config.ts
@@ -1,7 +1,7 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authCodeFlowConfig: AuthConfig = {
-    issuer: 'http://localhost:8080/auth/realms/PW',
+    issuer: 'http://localhost:8080/auth/realms/Nerdearla',
     redirectUri: window.location.origin,
     clientId: 'angular-demo-app',
     responseType: 'code',
